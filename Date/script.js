87c5f78474efaae2f033230e872ed424f6afeaa9
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-03-17 19:49:24 +0530

    Date And Polyfill Gothrough

---
+++ Date/script.js
@@ -0,0 +1,8 @@
+let now = new Date();
+console.log("Date :", now.getDate());
+console.log("Month :", now.getMonth());
+console.log("Year :", now.getFullYear());
+console.log("Day : ", now.getDay());
+console.log("Hours :", now.getHours());
+console.log("Minutes :", now.getMinutes());
+console.log("Seconds :", now.getSeconds());
