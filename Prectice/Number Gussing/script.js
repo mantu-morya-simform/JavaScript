678328095f8ddc5ce91b523910affaf9e9272a6e
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-03-30 19:33:46 +0530

    fixed: number-gussing-game win and lose part and reset part also

--- Prectice/Number Gussing/script.js
+++ Prectice/Number Gussing/script.js
@@ -1,4 +1,6 @@
 let remaningChance = 10;
+let winCount = 0;
+let loseCount = 0;
 
 const randomNumber = () => {
   let number = Math.floor(Math.random() * 100);
@@ -11,7 +13,8 @@ const click = (e) => {
   let inputText = document.querySelector(".input_text");
   let MessageText = document.querySelector(".message");
   let remaningChanceText = document.querySelector(".remaning_chance");
-  remaningChanceText.textContent = `Remaning Chance : ${remaningChance}`;
+  let winPointText = document.querySelector(".win_para");
+  let losePointText = document.querySelector(".lose_para");
 
   if (!inputText.value) {
     MessageText.textContent = `Provide An Input`;
@@ -21,45 +24,68 @@ const click = (e) => {
     let provideValue = Number(inputText.value);
     console.log(guessedNumber, provideValue);
 
-    if (remaningChance == 0 || remaningChance < 0) {
-      MessageText.textContent = `Your Chance Is No More Reset The Game to Play Again`;
-      MessageText.style.cssText = "color:#c92a2a; font-weight:600";
-      return;
-    }
+    // if (remaningChance == 0) {
+    //   MessageText.textContent = `Your Chance Is No More Reset The Game to Play Again`;
+    //   MessageText.style.cssText = "color:#c92a2a; font-weight:600";
+    //   return;
+    // }
 
     if (provideValue > guessedNumber) {
-      MessageText.textContent = `Provide Number is Grator! Guess a Smaller Number!`;
+      MessageText.textContent = `Provide Number is Greater! Guess a Smaller Number!`;
       MessageText.style.cssText = "color:#c92a2a; font-weight:600";
       remaningChance--;
     } else if (provideValue < guessedNumber) {
-      MessageText.textContent = `Provide Number is Smaller! Guess a Grator Number!`;
+      MessageText.textContent = `Provide Number is Smaller! Guess a Greater Number!`;
       MessageText.style.cssText = "color:#c92a2a; font-weight:600";
       remaningChance--;
     } else {
+      reset();
       MessageText.textContent = `You Guessed Exact Number!`;
       MessageText.style.cssText = "color:#40c057; font-weight:600";
+      winCount++;
+      winPointText.textContent = `Win : ${winCount}`;
+      guessedNumber = randomNumber();
     }
   } else {
     MessageText.textContent = `Provide Input As Valid Number`;
     MessageText.style.cssText = "color:#c92a2a; font-weight:600";
   }
+
+  if (remaningChance === 0) {
+    loseCount++;
+    losePointText.textContent = `Lose : ${loseCount}`;
+    reset();
+  }
+
+  remaningChanceText.textContent = `Remaning Chance : ${remaningChance}`;
 };
 guessBtn.addEventListener("click", click);
 
 let resetBtn = document.querySelector(".reset_btn");
 
-const reset = (e) => {
+const reset = () => {
+  let inputText = document.querySelector(".input_text");
+  let MessageText = document.querySelector(".message");
+  let remaningChanceText = document.querySelector(".remaning_chance");
+
+  inputText.value = "";
+  MessageText.textContent = "";
+  remaningChance = 10;
+  remaningChanceText.textContent = `Remaning Chance : ${remaningChance}`;
+};
+
+const resetGame = () => {
   const userResponse = confirm("Are you sure?");
 
   if (userResponse) {
-    let inputText = document.querySelector(".input_text");
-    let MessageText = document.querySelector(".message");
-    let remaningChanceText = document.querySelector(".remaning_chance");
-    inputText.textContent = "";
-    inputText.value = "";
-    MessageText.textContent = "";
-    remaningChance = 10;
-    remaningChanceText.textContent = `Remaning Chance : ${remaningChance}`;
+    let winPointText = document.querySelector(".win_para");
+    let losePointText = document.querySelector(".lose_para");
+    reset();
+    winCount = 0;
+    loseCount = 0;
+    winPointText.textContent = `Win : ${winCount}`;
+    losePointText.textContent = `Lose : ${loseCount}`;
   }
 };
-resetBtn.addEventListener("click", reset);
+
+resetBtn.addEventListener("click", resetGame);
